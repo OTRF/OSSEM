f7cd4c62ff73ec0cff7d16e6bc6291a8eae2901d
Roberto Rodriguez <rrodri0622@gmail.com>
2020-06-25 13:19:27 -0400

    updated all ds file and script

---
+++ resources/tools/ossemATTCKDM.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Author: Roberto Rodriguez (@Cyb3rWard0g)
+# License: GPLv3
+# Reference:
+
+import yaml
+import glob
+from os import path
+
+ds_files = glob.glob(path.join(path.dirname(__file__), '../../attack_data_sources/event-mappings', "*.yml"))
+ds_loaded = [yaml.safe_load(open(yf).read()) for yf in ds_files]
+
+all_data_sources = {'ds': []}
+
+for ds in ds_loaded:
+    all_data_sources['ds'].extend(ds)
+
+with open(r'../../attack_data_sources/event-mappings/all_data_sources.yml', 'w') as file:
+    yaml.dump(all_data_sources['ds'], file, sort_keys=False)
